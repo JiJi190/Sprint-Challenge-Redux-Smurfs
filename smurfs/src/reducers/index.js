7023a8c7eafcc72bbf31e528091e2aa66d20ca8e
unknown <robertrey1234@gmail.com>
2019-06-21 12:38:59 -0500

    Content displays, doesn't add smurfs to list. Honestly not sure what I'm typing at this point.

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,48 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { SMURFS_REQUEST, SMURFS_SUCCESS, SMURFS_FAILURE } from "../actions";
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ */
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addSmurf: false,
+  //  updatingSmurf: false
+  //  deletingSmurf: false
    error: null
  }
-*/
+
+ export default (state = initialState, action) => {
+   switch (action.type) {
+    case SMURFS_REQUEST:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: null
+      };
+
+      case SMURFS_SUCCESS:
+        return {
+          ...state,
+          fetchingSmurfs: false,
+          smurfs: action.payload,
+        };
+
+        case SMURFS_FAILURE:
+          return {
+            ...state,
+            error: action.payload,
+            fetchingSmurfs: false,
+          };
+
+          default:
+            return state;
+   }
+ }
+
 
 /*
   You'll only need one smurf reducer for this project.
